b71228db1a6e0666b90c3d0f6a756f3e1e651de9
Anthony Whyte <anthwhyte@gmail.com>
2018-12-12 15:32:27 -0500

    Switch to artist display name; create and populate artwork_artist table.

--- scripts/input/sql/met_art.sql
+++ scripts/input/sql/met_art.sql
@@ -2,8 +2,10 @@
 -- 1.0 Setup. Delete tables after every build iteration.
 --
 SET FOREIGN_KEY_CHECKS=0;
-DROP TABLE IF EXISTS artwork, artwork_type, classification, city, country, department,
-                     region, repository, temp_artwork, temp_city, temp_classification, temp_region;
+DROP TABLE IF EXISTS artist, artwork, artwork_artist, artwork_attribution, artist_role,
+                     artwork_type, classification, city, country, department, region, repository,
+                     temp_artwork, temp_artwork_artist, temp_artwork_artist_attribution,
+                     temp_artwork_artist_role, temp_city, temp_classification, temp_region;
 SET FOREIGN_KEY_CHECKS=1;
 
 --
@@ -330,7 +332,6 @@ LOAD DATA LOCAL INFILE './output/met_artwork/met_artwork-trimmed.csv'
 INTO TABLE temp_artwork
   CHARACTER SET utf8mb4
   FIELDS TERMINATED BY '\t'
-  -- FIELDS TERMINATED BY ','
   ENCLOSED BY '"'
   LINES TERMINATED BY '\n'
   IGNORE 1 LINES
@@ -477,3 +478,204 @@ SELECT ta.accession_number,
        LEFT JOIN repository repo
               ON TRIM(ta.repository_name) = TRIM(repo.repository_name)
  ORDER BY ta.temp_artwork_id;
+
+--
+-- 3.3 Artist table
+--
+ CREATE TABLE IF NOT EXISTS artist (
+  artist_id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+  artist_display_name VARCHAR(300) NOT NULL UNIQUE,
+  PRIMARY KEY (artist_id)
+)
+ENGINE=InnoDB
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_0900_ai_ci;
+-- CHARACTER SET latin1
+-- COLLATE latin1_swedish_ci;
+
+LOAD DATA LOCAL INFILE './output/met_artwork/met_artists_unique.csv'
+INTO TABLE artist
+  CHARACTER SET utf8mb4
+  FIELDS TERMINATED BY '\t'
+  ENCLOSED BY '"'
+  LINES TERMINATED BY '\n'
+  IGNORE 1 LINES
+  (artist_display_name);
+
+--
+-- 3.4 Temp artist role table
+-- WARN: 2nd row empty value (IGNORE 2 LINES)
+--
+CREATE TABLE IF NOT EXISTS artist_role (
+  artist_role_id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+  artist_role VARCHAR(100) NOT NULL UNIQUE,
+  PRIMARY KEY (artist_role_id)
+)
+ENGINE=InnoDB
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_0900_ai_ci;
+-- CHARACTER SET latin1
+-- COLLATE latin1_swedish_ci;
+
+LOAD DATA LOCAL INFILE './output/met_artwork/met_roles_unique.csv'
+INTO TABLE artist_role
+  CHARACTER SET utf8mb4
+  FIELDS TERMINATED BY '\t'
+  ENCLOSED BY '"'
+  LINES TERMINATED BY '\n'
+  IGNORE 2 LINES
+  (artist_role);
+
+--
+-- 3.5 Temp artwork_artist M2M junction table
+--
+--
+CREATE TABLE IF NOT EXISTS temp_artwork_artist_role (
+  temp_artwork_artist_role_id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+  accession_number VARCHAR(50) NOT NULL,
+  artwork_artist_index INTEGER NOT NULL,
+  artist_role VARCHAR(100) NULL,
+  PRIMARY KEY (temp_artwork_artist_role_id)
+)
+ENGINE=InnoDB
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_0900_ai_ci;
+-- CHARACTER SET latin1
+-- COLLATE latin1_swedish_ci;
+
+LOAD DATA LOCAL INFILE './output/met_artwork/met_artwork-artwork_roles-split.csv'
+INTO TABLE temp_artwork_artist_role
+  CHARACTER SET utf8mb4
+  FIELDS TERMINATED BY ','
+  ENCLOSED BY '"'
+  LINES TERMINATED BY '\n'
+  IGNORE 1 LINES
+  (accession_number, artwork_artist_index, artist_role);
+
+--
+-- 3.6 artwork attribution table
+--
+CREATE TABLE IF NOT EXISTS artwork_attribution (
+  artwork_attribution_id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+  artwork_attribution VARCHAR(100) NOT NULL UNIQUE,
+  PRIMARY KEY (artwork_attribution_id)
+)
+ENGINE=InnoDB
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_0900_ai_ci;
+-- CHARACTER SET latin1
+-- COLLATE latin1_swedish_ci;
+
+LOAD DATA LOCAL INFILE './output/met_artwork/met_attribution_unique.csv'
+INTO TABLE artwork_attribution
+  CHARACTER SET utf8mb4
+  FIELDS TERMINATED BY '\t'
+  ENCLOSED BY '"'
+  LINES TERMINATED BY '\n'
+  IGNORE 1 LINES
+  (artwork_attribution);
+
+--
+-- 3.7 Temp artwork_artist_attribution M2M junction table
+--
+--
+CREATE TABLE IF NOT EXISTS temp_artwork_artist_attribution (
+  temp_artwork_artist_attribution_id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+  accession_number VARCHAR(50) NOT NULL,
+  artwork_artist_index INTEGER NOT NULL,
+  artwork_attribution VARCHAR(100) NULL,
+  PRIMARY KEY (temp_artwork_artist_attribution_id)
+)
+ENGINE=InnoDB
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_0900_ai_ci;
+-- CHARACTER SET latin1
+-- COLLATE latin1_swedish_ci;
+
+LOAD DATA LOCAL INFILE './output/met_artwork/met_artwork-artwork_attribution-split.csv'
+INTO TABLE temp_artwork_artist_attribution
+  CHARACTER SET utf8mb4
+  FIELDS TERMINATED BY ','
+  ENCLOSED BY '"'
+  LINES TERMINATED BY '\n'
+  IGNORE 1 LINES
+  (accession_number, artwork_artist_index, artwork_attribution);
+
+--
+-- 3.8 Temp artwork_artist M2M junction table
+--
+CREATE TABLE IF NOT EXISTS temp_artwork_artist (
+  temp_artwork_artist_id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+  accession_number VARCHAR(50) NOT NULL,
+  artwork_artist_index INTEGER NOT NULL,
+  artist_display_name VARCHAR(300) NOT NULL,
+  PRIMARY KEY (temp_artwork_artist_id)
+)
+ENGINE=InnoDB
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_0900_ai_ci;
+-- CHARACTER SET latin1
+-- COLLATE latin1_swedish_ci;
+
+LOAD DATA LOCAL INFILE './output/met_artwork/met_artwork-artwork_artists-split.csv'
+INTO TABLE temp_artwork_artist
+  CHARACTER SET utf8mb4
+  FIELDS TERMINATED BY ','
+  ENCLOSED BY '"'
+  LINES TERMINATED BY '\n'
+  IGNORE 1 LINES
+  (accession_number, artwork_artist_index, artist_display_name);
+
+--
+-- 3.9 Temp artwork_artist M2M junction table
+-- WARNING: not all artwork is associated with an artist.
+--
+CREATE TABLE IF NOT EXISTS artwork_artist (
+  artwork_artist_id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+  artwork_id INTEGER NOT NULL,
+  artwork_attribution_id INTEGER NULL,
+  artwork_artist_index INTEGER NOT NULL,
+  artist_id INTEGER NOT NULL,
+  artist_role_id INTEGER NULL,
+  PRIMARY KEY (artwork_artist_id),
+  FOREIGN KEY (artwork_id) REFERENCES artwork(artwork_id)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (artwork_attribution_id) REFERENCES artwork_attribution(artwork_attribution_id)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (artist_id) REFERENCES artist(artist_id)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (artist_role_id) REFERENCES artist_role(artist_role_id)
+    ON DELETE CASCADE ON UPDATE CASCADE
+)
+ENGINE=InnoDB
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_0900_ai_ci;
+-- CHARACTER SET latin1
+-- COLLATE latin1_swedish_ci;
+
+INSERT IGNORE INTO artwork_artist
+(
+  artwork_id,
+  artwork_attribution_id,
+  artwork_artist_index,
+  artist_id,
+  artist_role_id
+)
+SELECT art.artwork_id, aa.artwork_attribution_id, taa.artwork_artist_index,
+       artist.artist_id, ar.artist_role_id
+  FROM temp_artwork_artist taa
+       LEFT JOIN artwork art
+              ON TRIM(taa.accession_number) = TRIM(art.accession_number)
+			 LEFT JOIN artist artist
+              ON TRIM(taa.artist_display_name) = TRIM(artist.artist_display_name)
+			 LEFT JOIN temp_artwork_artist_role taar
+              ON TRIM(taa.accession_number) = TRIM(taar.accession_number)
+					   AND taa.artwork_artist_index = taar.artwork_artist_index
+       LEFT JOIN artist_role ar
+              ON TRIM(taar.artist_role) = TRIM(ar.artist_role)
+       LEFT JOIN temp_artwork_artist_attribution taaa
+              ON TRIM(taa.accession_number) = TRIM(taaa.accession_number)
+             AND taa.artwork_artist_index = taaa.artwork_artist_index
+       LEFT JOIN artwork_attribution aa
+              ON TRIM(taaa.artwork_attribution) = TRIM(aa.artwork_attribution)
+ ORDER BY artwork_id, artwork_artist_index;
